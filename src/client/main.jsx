03a7cb189749bc36d2c512510a91815ca10f23c8
Terminat0r1 <139095405+Terminat0r1@users.noreply.github.com>
2023-11-22 13:42:35 -0800

    Initial commit

---
+++ src/client/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.less";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import AuthForm from "./features/auth/AuthForm";
+import Tasks from "./features/tasks/Tasks";
+import Root from "./layout/Root.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <Tasks /> },
+      { path: "/tasks", element: <Tasks /> },
+      { path: "/login", element: <AuthForm /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/server/prisma/seed.js
@@ -0,0 +1,30 @@
+const prisma = require("../prisma");
+
+/** Seeds the database with a user and some tasks */
+const seed = async () => {
+  await prisma.user.upsert({
+    where: {
+      username: "foo",
+    },
+    update: {},
+    create: {
+      username: "foo",
+      password: "bar",
+      tasks: {
+        create: [
+          { description: "task 1" },
+          { description: "task 2" },
+          { description: "task 3" },
+        ],
+      },
+    },
+  });
+};
+
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
