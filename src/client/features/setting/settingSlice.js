3a7047cf1c9e0873fb193b6650ef61488614c795
Home PC <vfxprojectguy@gmail.com>
2023-12-06 23:26:33 -0800

    continue work on setting page

---
+++ src/client/features/setting/settingSlice.js
@@ -0,0 +1,80 @@
+import { createSlice } from "@reduxjs/toolkit";
+import api from "../../store/api";
+
+/** Authentication endpoints */
+const postApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    updateUsername: builder.mutation({
+      query: (credentials) => ({
+        url: "/users/update-username",
+        method: "PUT",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data,
+    }),
+    //-----
+    updateEmail: builder.mutation({
+      query: (credentials) => ({
+        url: "/users/update-email",
+        method: "PUT",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data,
+    }),
+    //-----------
+    updatebirthDate: builder.mutation({
+      query: (credentials) => ({
+        url: "/users/update-birthdate",
+        method: "PUT",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data,
+    }),
+    //---------------------
+    updatelocation: builder.mutation({
+      query: (credentials) => ({
+        url: "/users/update-location",
+        method: "PUT",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data,
+    }),
+    //---------------------
+    updatephoto: builder.mutation({
+      query: (credentials) => ({
+        url: "/users/update-profile-photo",
+        method: "PUT",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data,
+    }),
+    //---------------------
+    updatePassword: builder.mutation({
+      query: (credentials) => ({
+        url: "/users/update-password",
+        method: "PUT",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data,
+    }),
+
+    //---------------------
+    deleteUser: builder.mutation({
+      query: () => ({
+        url: "/users/profile",
+        method: "DELETE",
+      }),
+      transformErrorResponse: (response) => response.data,
+    }),
+  }),
+});
+
+export const {
+  useUpdateUsernameMutation,
+  useUpdateEmailMutation,
+  useUpdatebirthDateMutation,
+  useUpdatelocationMutation,
+  useUpdatephotoMutation,
+  useUpdatePasswordMutation,
+  useDeleteUserMutation,
+} = postApi;
